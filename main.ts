32e12b1141fa2c7aed59eeaab874072a162927cd
Robotsrule4545 <77372650+Robotplaze@users.noreply.github.com>
2022-05-29 12:05:25 +0800

    Added a powerup, and also extended the code. Fixed bugs.

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 namespace SpriteKind {
     export const boss = SpriteKind.create()
     export const bossprite = SpriteKind.create()
+    export const powerup = SpriteKind.create()
 }
 namespace StatusBarKind {
     export const Ammo = StatusBarKind.create()
@@ -13,27 +14,35 @@ statusbars.onZero(StatusBarKind.Health, function (status) {
     music.stopAllSounds()
 })
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    mySprite.startEffect(effects.fire, 100)
+    otherSprite.startEffect(effects.fire, 100)
     statusbar.value += -10
     info.changeScoreBy(1)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.powerup, function (sprite, otherSprite) {
+    boolean1 = 1
+    otherSprite.destroy()
+    machinegunammo = 50
+})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite3, otherSprite3) {
-    Enemy1.startEffect(effects.fire, 100)
-    Enemy1.destroy()
+    otherSprite3.startEffect(effects.fire, 100)
+    otherSprite3.destroy()
     music.powerUp.play()
     info.changeScoreBy(1)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.boss, function (sprite2, otherSprite2) {
-    boss1.startEffect(effects.halo, 100)
-    boss1.destroy()
+    otherSprite2.startEffect(effects.halo, 100)
+    otherSprite2.destroy()
     music.jumpUp.play()
     info.changeScoreBy(10)
 })
-let projectile: Sprite = null
+let machinegunpowerup: Sprite = null
 let projectile2: Sprite = null
-let counterboss = 0
 let boss1: Sprite = null
+let projectile: Sprite = null
+let counterboss = 0
 let Enemy1: Sprite = null
+let machinegunammo = 0
+let boolean1 = 0
 let statusbar: StatusBarSprite = null
 let mySprite: Sprite = null
 game.showLongText("Hit A to start!", DialogLayout.Bottom)
@@ -190,12 +199,6 @@ statusbar.attachToSprite(mySprite, 5, 0)
 statusbar.setColor(2, 5)
 statusbar.setLabel("HP", 1)
 statusbar.value = 100
-if (info.score() == 50) {
-    game.over(true, effects.confetti)
-    game.showLongText("You did it!!!", DialogLayout.Bottom)
-    music.powerUp.play()
-    music.stopAllSounds()
-}
 game.onUpdateInterval(1000, function () {
     Enemy1 = sprites.create(img`
         ...........fffffc...........
@@ -221,12 +224,63 @@ game.onUpdateInterval(1000, function () {
     Enemy1.y = 0
     counterboss += 1
 })
+forever(function () {
+    if (boolean1 == 1) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . 
+            . . . . . . . . 
+            . . . . . . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . . . . . . 
+            `, mySprite, 0, -100)
+        music.pewPew.play()
+        machinegunammo += -1
+        if (machinegunammo <= 0) {
+            projectile = sprites.createProjectileFromSprite(img`
+                . . . . . . . . 
+                . . . . . . . . 
+                . . . . . . . . 
+                . . . 1 1 . . . 
+                . . . 1 1 . . . 
+                . . . 1 1 . . . 
+                . . . 1 1 . . . 
+                . . . . . . . . 
+                `, mySprite, 0, -100)
+            music.pewPew.play()
+            pause(100)
+        }
+    } else {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . 
+            . . . . . . . . 
+            . . . . . . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . 1 1 . . . 
+            . . . . . . . . 
+            `, mySprite, 0, -100)
+        music.pewPew.play()
+        pause(100)
+    }
+})
+forever(function () {
+    if (info.score() >= 50) {
+        game.over(true, effects.confetti)
+        game.showLongText("You did it!!!", DialogLayout.Bottom)
+        music.powerUp.play()
+        music.stopAllSounds()
+    }
+})
 game.onUpdateInterval(500, function () {
     if (counterboss == 10) {
         sprites.destroyAllSpritesOfKind(SpriteKind.Enemy, effects.rings, 100)
         game.showLongText("A boss has appeared! Be careful!", DialogLayout.Bottom)
         boss1 = sprites.create(assets.image`Boss`, SpriteKind.boss)
-        boss1.setPosition(randint(0, 100), 5)
+        boss1.setPosition(randint(0, 100), 25)
         boss1.setBounceOnWall(true)
         boss1.setVelocity(50, 50)
         music.siren.play()
@@ -255,16 +309,26 @@ game.onUpdateInterval(500, function () {
         projectile2.follow(mySprite)
     }
 })
-game.onUpdateInterval(200, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . . . . . . 
-        . . . 1 1 . . . 
-        . . . 1 1 . . . 
-        . . . 1 1 . . . 
-        . . . 1 1 . . . 
-        . . . . . . . . 
-        `, mySprite, 0, -100)
-    music.pewPew.play()
+game.onUpdateInterval(10000, function () {
+    machinegunpowerup = sprites.create(img`
+        . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 f 1 1 1 1 1 1 1 
+        1 1 1 f 1 1 1 1 f f f 1 1 1 1 1 
+        1 1 f f f 1 1 1 1 1 f 1 1 1 1 1 
+        1 1 1 f 1 1 1 1 1 f 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+        . 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+        . . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+        `, SpriteKind.powerup)
+    machinegunpowerup.setPosition(randint(0, 150), 0)
+    machinegunpowerup.setVelocity(0, 50)
+    machinegunpowerup.setFlag(SpriteFlag.AutoDestroy, false)
 })
